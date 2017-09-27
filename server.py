b7f57f19997d4c237f7c1c469f290776f32683e6
RiflerRick <rajdeep.mukherjee@st.niituniversity.in>
2017-09-27 17:11:40 +0530

    consumer thread and producer thread added

--- server.py
+++ server.py
@@ -29,29 +29,84 @@ LOG_FILEPATH = settings.LOG_FILEPATH
 LOG_LEVEL = settings.LOG_LEVEL
 SERVER_MAX_QUEUED_CON = settings.SERVER_MAX_QUEUED_CON
 TOPICS = settings.TOPICS
+CLIENT_PUBLISHER = settings.CLIENT_PUBLISHER
+CLIENT_SUBSCRIBER = settings.CLIENT_SUBSCRIBER
+MAX_MESSAGE_SIZE = settings.MAX_MESSAGE_SIZE
+SERVER_ACKNOWLEDGEMENT = settings.SERVER_ACKNOWLEDGEMENT
+CLIENT_SHUTDOWN_SIGNAL = settings.CLIENT_SHUTDOWN_SIGNAL
+CONSUMER_REQUEST_WORD = settings.CONSUMER_REQUEST_WORD
 
 logging.basicConfig(filename=LOG_FILEPATH, level=LOG_LEVEL)
 logger = logging.getLogger("bbmq_module")
 
-class ClientThread(threading.Thread):
+
+class ProduerThread(threading.Thread):
     """
     Connection thread will be waiting for connections from producers or consumers
     """
-    def __init__(self, thread_id, thread_counter):
+    def __init__(self, producer_socket, inbound_socket_address, queue):
         """
-        initialize the thread
+        initialize the thread. During initialization of this thread, it must confirm to the
+        producer that the producer can now start communication
         :param thread_id:
         :param thread_counter:
         """
         threading.Thread.__init__(self)
-        self.thread_id = thread_id
-        self.thread_counter = thread_counter
+        self.logger = logging.getLogger("bbmq_module.BBMQServer.ProducerThread_".format(
+            inbound_socket_address))
+        self.socket = producer_socket
+        self.queue = queue
+        self.socket.send(SERVER_ACKNOWLEDGEMENT)
 
     def run(self):
         """
         run the thread. called when the start() method of Thread super class is called
         :return:
         """
+        while True:
+            message = self.socket.recv(MAX_MESSAGE_SIZE)
+            if message == CLIENT_SHUTDOWN_SIGNAL:
+                break
+            self.logger.info("Received payload")
+            self.logger.info("Publishing to queue")
+            self.queue.add_message(message)
+        self.socket.close()
+
+
+class ConsumerThread(threading.Thread):
+    """
+    Connection thread will be waiting for connections from producers or consumers
+    """
+
+    def __init__(self, consumer_socket, inbound_socket_address, queue):
+        """
+        initialize the thread
+        :param consumer_socket:
+        :param inbound_socket_address:
+        :param queue:
+        """
+        threading.Thread.__init__(self)
+        self.logger = logging.getLogger("bbmq_module.BBMQServer.ProducerThread_".format(
+            inbound_socket_address))
+        self.socket = consumer_socket
+        self.queue = queue
+        self.socket.send(SERVER_ACKNOWLEDGEMENT)
+
+    def run(self):
+        """
+        run the thread. called when the start() method of Thread super class is called
+        :return:
+        """
+        while True:
+            request = self.socket.recv(MAX_MESSAGE_SIZE)
+            if request == CLIENT_SHUTDOWN_SIGNAL:
+                break
+            if request == CONSUMER_REQUEST_WORD:
+                self.logger.info("Received request for new message")
+                self.logger.info("Fetching from queue")
+                message = self.queue.fetch_message()
+                self.socket.send(message)
+        self.socket.close()
 
 
 class ConnectionThread(threading.Thread):
@@ -59,18 +114,18 @@ class ConnectionThread(threading.Thread):
     Connection thread will be waiting for connections from producers or consumers
     """
 
-    def __init__(self, thread_id, thread_counter, server_socket, connection_queue):
+    def __init__(self, server_socket, connection_queue, topics):
         """
         initialize the thread
-        :param thread_id:
-        :param thread_counter:
+        :param server_socket:
+        :param connection_queue:
+        :param topics: list of available topics that clients can publish/subscribe to
         """
         threading.Thread.__init__(self)
         self.logger = logging.getLogger("bbmq_module.BBMQServer.ConnectionThread")
-        self.thread_id = thread_id
-        self.thread_counter = thread_counter
         self.sock = server_socket
         self.connection_queue = connection_queue
+        self.topics = topics
 
     def run(self):
         """
@@ -86,10 +141,15 @@ class ConnectionThread(threading.Thread):
             try:
                 client_type = ast.literal_eval(client_metadata)["type"]
                 client_topic = ast.literal_eval(client_metadata)["topic"]
-                if client_type == "producer":
+                if client_topic not in self.topics:
+                    self.logger.info("Client '{}' has subscribed to a non-existing"
+                                     " topic {}".format(inbound_socket_address, client_topic))
+                    socket_connection.close()
+                    continue
+                if client_type == CLIENT_PUBLISHER:
                     self.logger.info("Client is a producer and will publish to queue:"
                                      " {}".format(client_topic))
-                elif client_type == "consumer":
+                elif client_type == CLIENT_SUBSCRIBER:
                     self.logger.info("Client is a consumer and will subscribe to queue:"
                                      " {}".format(client_topic))
                 else:
@@ -162,6 +222,29 @@ class BBMQServer(object):
         }
         return 0
 
+    def get_topic_queue(self, topic_name):
+        """
+        gets the queue instance for a topic
+        :param topic_name:
+        :return:
+        """
+        if topic_name not in self.topics.keys():
+            return -1
+        return self.topics[topic_name]["queue"]
+
+    def update_topic(self, topic_name, producer, consumer):
+        """
+        update the topic with new producers and consumers
+        :param topic_name:
+        :param producers: tuple ()
+        :param consumers: tuple ()
+        :return:
+        """
+        if producer == None:
+            self.topics[topic_name]["consumers"].append(consumer)
+        else:
+            self.topics[topic_name]["producers"].append(producer)
+
     def create_queue(self):
         """
         create an custom queue instance and return it
@@ -177,9 +260,27 @@ class BBMQServer(object):
         :return:
         """
         self.logger.info("Starting connection thread")
-        self.connection_thread = ConnectionThread(0, 1, self.sock, self.connection_queue)
+        self.connection_thread = ConnectionThread(self.sock, self.connection_queue,
+                                                  self.topics.keys())
         self.connection_thread.start()
         self.logger.info("Connection thread started")
+
+    def spawn_producer_thread(self, producer_socket, inbound_socket_address, queue):
+        """
+        spawns a producer thread to publish to the queue
+        :param inbound_socket_address:
+        :param queue:
+        :return:
+        """
+
+
+    def spawn_consumer_thread(self, consumer_socket, inbound_socket_address, queue):
+        """
+        spawns a consumer thread to subscribe to the queue
+        :param inbound_socket_address:
+        :param queue:
+        :return:
+        """
 
 
 def main():
@@ -198,6 +299,29 @@ def main():
     server.spawn_connection_thread()
     while True:
         connection = server.connection_queue.get(block=True)
+        # The connection will be a dictionary of the following format
+        # {
+        #     "client_type": client_type,
+        #     "client_topic": client_topic,
+        #     "socket": socket_connection,
+        #     "inbound_socket_address": inbound_socket_address
+        # }
+        client_type = connection["client_type"]
+        client_topic = connection["client_topic"]
+        client_socket = connection["socket"]
+        inbound_socket_address = connection["inbound_socket_address"]
+        queue = server.get_topic_queue(topic_name=client_topic)
+        if client_type == CLIENT_PUBLISHER:
+            server.update_topic(client_topic, (client_socket, inbound_socket_address), None)
+            server.spawn_producer_thread(client_socket, inbound_socket_address, queue)
+        else:
+            server.update_topic(client_topic, None, (client_socket, inbound_socket_address))
+            server.spawn_consumer_thread(client_socket, inbound_socket_address, queue)
+
+
+
+
+
 
 
 
