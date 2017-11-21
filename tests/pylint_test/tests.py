2db9464d00343360e85bb9ea7dd97ad09baafa81
RiflerRick <rajdeep.mukherjee@st.niituniversity.in>
2017-11-21 13:05:25 +0530

    change in lint test

--- tests/pylint_test/tests.py
+++ tests/pylint_test/tests.py
@@ -6,31 +6,31 @@ PASS_VAL = 5
 def test_server():
 
     results = Run(['../../server.py'], exit=False)
-    a = int(results.linter.stats['global_note'])
+    a = float(results.linter.stats['global_note'])
     assert (a > PASS_VAL)
 
 def test_server_daemon():
 
     results = Run(['../../server_daemon.py'], exit=False)
-    a = int(results.linter.stats['global_note'])
+    a = float(results.linter.stats['global_note'])
     assert (a > PASS_VAL)
 
 def test_bbmq_server():
     
     results = Run(['../../bbmq_server.py'], exit=False)
-    a = int(results.linter.stats['global_note'])
+    a = float(results.linter.stats['global_note'])
     assert (a > PASS_VAL)
 
 def test_producer():
 
     results = Run(['../../producer.py'], exit=False)
-    a = int(results.linter.stats['global_note'])
+    a = float(results.linter.stats['global_note'])
     assert (a > PASS_VAL)
 
 def test_consumer():
 
     results = Run(['../../consumer.py'], exit=False)
-    a = int(results.linter.stats['global_note'])
+    a = float(results.linter.stats['global_note'])
     assert (a > PASS_VAL)
 
 
