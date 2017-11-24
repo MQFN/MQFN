f8a5f5a9c2ef41e74e15fb707054cc1a42b43b33
RiflerRick <rajdeep.mukherjee@st.niituniversity.in>
2017-11-24 12:33:17 +0530

    bug fix

--- bbmq/parition_messages.py
+++ bbmq/parition_messages.py
@@ -3,24 +3,24 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import settings
 import textwrap
 
-MESSAGE_HEAD_SECRET = settings.MESSAGE_HEAD_SECRET
-MESSAGE_TAIL_SECRET = settings.MESSAGE_TAIL_SECRET
+HEAD = settings.HEAD
+TAIL = settings.TAIL
 PARTITION_SIZE = settings.PARTITION_SIZE
+
 
 class Message:
 
-    def __init__(self, message, size):
+    def __init__(self, message):
         """
         Initialize the Message class
         :param message:
-        :param n: size of the message
         """
         self.message = message
         self.i = 0
-        self.size = size
-        self.body_partitions = int(size/PARTITION_SIZE)
+        self.size = sys.getsizeof(message)
+        self.body_partitions = int(self.size/PARTITION_SIZE)
         # partitions the text into self.body_partitions number of partitions
-        self.partitions = textwrap.wrap(message, self.body_partitions)
+        self.partitions = textwrap.wrap(self.message, PARTITION_SIZE)
 
     def __iter__(self):
         """
@@ -35,11 +35,12 @@ class Message:
         :return:
         """
         if self.i == 0:
-            return MESSAGE_HEAD_SECRET
             self.i += 1
+            return HEAD
         else:
-            if self.i < self.body_partitions:
+            if self.i <= self.body_partitions:
                 self.i += 1
                 return self.partitions[self.i-1]
             else:
+                return TAIL
                 raise StopIteration()
